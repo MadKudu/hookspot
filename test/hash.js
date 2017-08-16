b9c0176d4712543ad646117186eda8290aca454c
Paul Cothenet <paul@madkudu.com>
2017-08-16 07:45:24 -0700

    Update code style

--- test/hash.js
+++ test/hash.js
@@ -1,16 +1,14 @@
-'use strict'
-
 const chai = require('chai')
 const expect = chai.expect
 
 const hash = require('../lib/hash')
 
-// load env variables from .env
 describe('hash', function () {
-  this.timeout(5000)
-
-  it('return a hash without error', () => {
+  it('should return an integer between 0 and 100', () => {
     const result = hash('1878712')
-    console.log(result)
+    expect(result).to.be.a('number')
+    expect(parseInt(result)).to.equal(result) // check if integer
+    expect(result).to.be.above(-1)
+    expect(result).to.be.below(101)
   })
 })
